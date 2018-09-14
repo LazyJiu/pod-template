74d18a78bc185df55ba7e70f9afc75d368826c54
shibin <248301344@qq.com>
2018-09-14 12:59:03 +0800

    修改模板

---
+++ templates/ios/Example/PROJECT/CPDaaaaa.h
@@ -0,0 +1,13 @@
+//
+//  CPDaaaaa.h
+//  PROJECT_Example
+//
+//  Created by 施斌 on 2018/9/14.
+//  Copyright © 2018年 PROJECT_OWNER. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CPDaaaaa : NSObject
+
+@end
---
+++ templates/ios/Example/PROJECT/CPDaaaaa.m
@@ -0,0 +1,13 @@
+//
+//  CPDaaaaa.m
+//  PROJECT_Example
+//
+//  Created by 施斌 on 2018/9/14.
+//  Copyright © 2018年 PROJECT_OWNER. All rights reserved.
+//
+
+#import "CPDaaaaa.h"
+
+@implementation CPDaaaaa
+
+@end
